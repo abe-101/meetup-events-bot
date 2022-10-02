cdc60790b91ad3e50c74efc2a93738f7ff79b2a8
Abe <abe@habet.dev>
2022-10-02 03:26:04 -0400

    add reaction emoji

--- dropdown.py
+++ dropdown.py
@@ -65,6 +65,82 @@ class Bot(commands.Bot):
         super().__init__(
             command_prefix=commands.when_mentioned_or("!"), intents=intents
         )
+        self.role_message_id = 1026030849252143144  # ID of the message that can be reacted to to add/remove a role.
+        self.emoji_to_role = {
+            discord.PartialEmoji(
+                name="🔴"
+            ): 1020163559264628796,  # ID of the role Chicago associated with unicode emoji '🔴'.
+            discord.PartialEmoji(
+                name="🟢"
+            ): 1020162194027069490,  # ID of the role Chicago associated with unicode emoji '🔴'.
+        }
+
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        """Gives a role based on a reaction emoji."""
+        # Make sure that the message the user is reacting to is the one we care about.
+        if payload.message_id != self.role_message_id:
+            return
+
+        guild = self.get_guild(payload.guild_id)
+        if guild is None:
+            # Check if we're still in the guild and it's cached.
+            return
+
+        try:
+            role_id = self.emoji_to_role[payload.emoji]
+        except KeyError:
+            # If the emoji isn't the one we care about then exit as well.
+            return
+
+        role = guild.get_role(role_id)
+        if role is None:
+            # Make sure the role still exists and is valid.
+            return
+
+        try:
+            # Finally, add the role.
+            await payload.member.add_roles(role)
+            print(f"{payload.member} joined {role}")
+        except discord.HTTPException:
+            # If we want to do something in case of errors we'd do it here.
+            pass
+
+    async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
+        """Removes a role based on a reaction emoji."""
+        # Make sure that the message the user is reacting to is the one we care about.
+        if payload.message_id != self.role_message_id:
+            return
+
+        guild = self.get_guild(payload.guild_id)
+        if guild is None:
+            # Check if we're still in the guild and it's cached.
+            return
+
+        try:
+            role_id = self.emoji_to_role[payload.emoji]
+        except KeyError:
+            # If the emoji isn't the one we care about then exit as well.
+            return
+
+        role = guild.get_role(role_id)
+        if role is None:
+            # Make sure the role still exists and is valid.
+            return
+
+        # The payload for `on_raw_reaction_remove` does not provide `.member`
+        # so we must get the member ourselves from the payload's `.user_id`.
+        member = guild.get_member(payload.user_id)
+        if member is None:
+            # Make sure the member still exists and is valid.
+            return
+
+        try:
+            # Finally, remove the role.
+            await member.remove_roles(role)
+            print(f"{member} left {role}")
+        except discord.HTTPException:
+            # If we want to do something in case of errors we'd do it here.
+            pass
 
     async def on_ready(self):
         print(f"Logged in as {self.user} (ID: {self.user.id})")
